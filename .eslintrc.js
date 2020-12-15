d9c4b071bb4b0aaecbc6305547bb3d5646e42d88
juicyjusung <juicyjusung@gmail.com>
2020-12-15 09:16:22 +0900

    chore: lint 옵션 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-vars': 'warn',
     'require-await': 'warn',
   },
 };
