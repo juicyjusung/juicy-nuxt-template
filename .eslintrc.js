8cdb2cb66987d439c944a209f2fc197ee0f72c17
juicyjusung <juicyjusung@gmail.com>
2020-12-15 09:05:42 +0900

    chore: 🔧 eslint 룰 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,9 @@ module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript', 'prettier', 'prettier/vue', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'require-await': 'warn',
+  },
 };
